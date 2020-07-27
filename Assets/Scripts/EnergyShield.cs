7f90c5881fac7a1afeb6969755cc245d7c26417a
Blake Barton <blake.barton@tamu.edu>
2020-07-26 19:38:46 -0500

    Shield power up creates energy shield
    Energy shield will lock its position to player. No functionality past that.

---
+++ Assets/Scripts/EnergyShield.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyShield : MonoBehaviour
+{
+    // config variables
+
+    // cached references
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LockPositionToPlayer();
+    }
+
+    private void LockPositionToPlayer()
+    {
+        transform.position = player.transform.position;
+    }
+}
---
+++ Assets/Scripts/ShieldPowerUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPowerUp : MonoBehaviour
+{
+    [SerializeField] GameObject energyShield;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Player player = collision.gameObject.GetComponent<Player>();    // get the player object
+        if (!player) { return; }                                        // return if the collision isn't a player
+
+        Instantiate(energyShield, player.transform.position, player.transform.rotation);
+    }
+}
