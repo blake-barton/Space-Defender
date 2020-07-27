a465ac2ea4a57c131a91887e0c2ffea6b0b5b1ea
Blake Barton <blake.barton@tamu.edu>
2020-07-27 10:50:15 -0500

    Energy shield now blocks incoming projectiles
    Can be configured with # of hits.

--- Assets/Scripts/EnergyShield.cs
+++ Assets/Scripts/EnergyShield.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class EnergyShield : MonoBehaviour
 {
     // config variables
+    [SerializeField] int hitsToDestroy = 3;
 
     // cached references
     Player player;
@@ -25,5 +26,16 @@ public class EnergyShield : MonoBehaviour
     private void LockPositionToPlayer()
     {
         transform.position = player.transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Energy shield hit: " + collision.gameObject.name + " | health = " + hitsToDestroy);
+        hitsToDestroy--;
+
+        if (hitsToDestroy <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
