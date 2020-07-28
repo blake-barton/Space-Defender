109e546ba423eb85be2a8f2794392af0389aa55a
Blake Barton <blake.barton@tamu.edu>
2020-07-27 22:14:50 -0500

    Added energy shield activate audio

--- Assets/Scripts/ShieldPowerUp.cs
+++ Assets/Scripts/ShieldPowerUp.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class ShieldPowerUp : MonoBehaviour
 {
     [SerializeField] GameObject energyShield;
+    [SerializeField] AudioClip activateAudio;
+    [SerializeField] [Range(0, 1)] float activateAudioVolume = .5f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Player player = collision.gameObject.GetComponent<Player>();    // get the player object
         if (!player) { return; }                                        // return if the collision isn't a player
+
+        // play activate audio
+        AudioSource.PlayClipAtPoint(activateAudio, Camera.main.transform.position, activateAudioVolume);
 
         Instantiate(energyShield, player.transform.position, player.transform.rotation);
     }
