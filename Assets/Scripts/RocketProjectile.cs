7fd4422e31b6bbcebe974dd8b4283b5a687aec9e
Blake Barton <blake.barton@tamu.edu>
2020-07-29 10:41:22 -0500

    Stopped rocket from playing sound if it misses player

--- Assets/Scripts/RocketProjectile.cs
+++ Assets/Scripts/RocketProjectile.cs
@@ -12,6 +12,11 @@ public class RocketProjectile : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Instantiate(explosionFX, transform.position, transform.rotation);
-        AudioSource.PlayClipAtPoint(explosionSound, Camera.main.transform.position, explosionSoundVolume);
+
+        // play sound if collision is not a shredder
+        if (!collision.gameObject.CompareTag("Shredder"))
+        {
+            AudioSource.PlayClipAtPoint(explosionSound, Camera.main.transform.position, explosionSoundVolume);
+        }
     }
 }
