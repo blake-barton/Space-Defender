23c8a9f4aa6969e78792eb1c6371fb819fdf8389
Blake Barton <blake.barton@tamu.edu>
2020-07-28 21:45:21 -0500

    Added sound effect to rocket explosion

--- Assets/Scripts/RocketProjectile.cs
+++ Assets/Scripts/RocketProjectile.cs
@@ -6,9 +6,12 @@ public class RocketProjectile : MonoBehaviour
 {
     // config variables
     [SerializeField] GameObject explosionFX;
+    [SerializeField] AudioClip explosionSound;
+    [SerializeField] float explosionSoundVolume = 0.5f;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Instantiate(explosionFX, transform.position, transform.rotation);
+        AudioSource.PlayClipAtPoint(explosionSound, Camera.main.transform.position, explosionSoundVolume);
     }
 }
