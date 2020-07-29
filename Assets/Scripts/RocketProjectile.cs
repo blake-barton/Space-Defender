2a38537a59d5d9be4c67b1c136c927b5bca7b236
Blake Barton <blake.barton@tamu.edu>
2020-07-28 21:08:39 -0500

    Rockets spawn explosion effect on collision

---
+++ Assets/Scripts/RocketProjectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketProjectile : MonoBehaviour
+{
+    // config variables
+    [SerializeField] GameObject explosionFX;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(explosionFX, transform.position, transform.rotation);
+    }
+}
